86f2ab26bb9232966e561cbb5048686b64bc4406
iQOOforwardbot <iQOOGitHubforwad1@gmail.com>
2024-12-01 20:54:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from os import environ
 class Config:
     API_ID = environ.get("API_ID", "")
     API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "8061980811:AAFvHG03cpua_f6EBGjr87W6_9-Jrm-5Hmw")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "")
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://forwardbot17:rvforward1@cluster0.ubuxz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_URI = environ.get("DATABASE", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7716635383').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002402141599'))
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/Magic_Botz") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
